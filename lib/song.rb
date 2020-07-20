6c5ae34e7b5cb3f34d7b61f96dcde4b8f20b3002
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-20 02:49:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,16 +34,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if !self.find_by_name(name)
-      self.create_by_name(name)
-    else
-      self.find_by_name(name)
-    end
+    !self.find_by_name(name) ? self.create_by_name(name) : self.find_by_name(name)
   end
 
   def self.alphabetical
     sorted_names = self.all.sort_by {|song| song.name}
-    #sorted_names
   end
 
   def self.new_from_filename(filename)
